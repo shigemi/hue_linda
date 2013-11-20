7894fe12285854f8eb09074b0cd29e2cbb302c9f
shigemi <shigemi@ht.sfc.keio.ac.jp>
2013-11-20 17:03:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ function init(){
         analyser.connect(audioContext.destination);
         //mediaStreamSource.connect(audioContext.destination);
 
+       //ループ
+        setInterval(Loop, 500);
         Loop();
     }
     //マイクの有無を調べる
@@ -55,55 +57,94 @@ function init(){
     }else{
         console.log("マイクデバイスがありません");
     }
-}
+ }
 
 
-function Loop(){
+ function Loop(){
     //Canvasをクリア
-    ctx.clearRect(0,0,w,h);
+     ctx.clearRect(0,0,w,h);
     //背景色
-    ctx.fillStyle = "#efefef";
+      ctx.fillStyle = "#efefef";
     //背景描画
-    ctx.fillRect(0,0,w,h);
+     ctx.fillRect(0,0,w,h);
 
     //符号なし8bitArrayを生成
-    var data = new Uint8Array(analyser.frequencyBinCount);
+     var data = new Uint8Array(analyser.frequencyBinCount);
     //周波数データ
-    analyser.getByteFrequencyData(data);
-
+     analyser.getByteFrequencyData(data);
     //周波数の色
-    ctx.fillStyle = "#ccc";
-	
-	//alert(data.length);
-	
-	/////ここから
-	
-	//if(data[50] > 100){
-	// alert("data[0]:"+data[0] +"  "+ "data[50]:"+data[50]+"  "+"data[100]:"+data[100]+"  "+"data[200]:"+data[200]+"  "+"data[500]:"+data[500]+"  ");
-	  
-	  //$(function(){
-         //$("#btn_send").click(function(){
-         //var m = $("#message").val();
-		 
-		 if(data[1000]>100)
-
-		 
-         ts.write(["hue","0","hsb","50000","200","200"]);
-        //});
-       //});
-	   
-	   
-	 //}
+      ctx.fillStyle = "#ccc";
 	 
 	 
-	//alert(data[50]);
+	 ////ここから判定＊＊＊＊
+	 
+	 
+	 
+      if(data[400] > 10){
+	    ts.write(["hue","0","hsb","65000","200","200"]);   
+		}
+         else if(data[360] > 10){
+	     ts.write(["hue","0","hsb","60000","200","200"]);
+	   }
+         else if(data[350] > 10){
+	      ts.write(["hue","0","hsb","58000","200","200"]);
+	   }
+        else if (data[340] > 10){
+	      ts.write(["hue","0","hsb","55000","200","200"]);
+	   }
+	    else if(data[330] > 10){
+	     ts.write(["hue","0","hsb","53000","200","200"]);
+	   }
+         else if(data[320] > 10){
+	      ts.write(["hue","0","hsb","50000","200","200"]);
+	   }
+         else if (data[310] > 10){
+	      ts.write(["hue","0","hsb","48000","200","200"]);
+	   }
+        else if(data[300] > 10){
+	      ts.write(["hue","0","hsb","45000","200","200"]);
+       }
+	   　else if(data[290] > 10){
+	     ts.write(["hue","0","hsb","42000","200","200"]);
+	   }
+         else if(data[270] > 10){
+	      ts.write(["hue","0","hsb","38000","200","200"]);
+	   }
+        else if (data[250] > 10){
+	      ts.write(["hue","0","hsb","30000","200","200"]);
+	   }
+        else if(data[230] > 10){
+	      ts.write(["hue","0","hsb","28000","200","200"]);
+       }
+	     else if (data[210] > 10){
+	      ts.write(["hue","0","hsb","25000","200","200"]);
+	   }
+        else if(data[190] > 10){
+	      ts.write(["hue","0","hsb","20000","200","200"]);
+       }
+	   　else if(data[170] > 10){
+	     ts.write(["hue","0","hsb","18000","200","200"]);
+	   }
+         else if(data[150] > 10){
+	      ts.write(["hue","0","hsb","13000","200","200"]);
+	   }
+	     else if (data[120] > 10){
+	      ts.write(["hue","0","hsb","8000","200","200"]);
+	   }
+        else if(data[80] > 10){
+	      ts.write(["hue","0","hsb","3000","200","200"]);
+       }
+        else{
+           ts.write(["hue","0","hsb","1000","200","200"]); 
+	   }
+	 //////ここまで＊＊＊＊
+	 
 
+       //グラフ描画
     for(var i = 0; i < data.length; ++i) {
-        //上部の描画
+       //上部の描画
         ctx.fillRect(i*5, 0, 5, data[i]);
         //下部の描画
         ctx.fillRect(i*5, h, 5, -data[i]);
-    }
-    //ループ
-    requestAnimationFrame(Loop);
+   }
 }
